ed250118847155b8416f49c174cc8311c903cc2e
Annema Cozmoz <mikewittorp@gmail.com>
2020-11-19 10:42:55 -0500

    Updated media screen max width so header does not disappear

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,13 @@
 		$banner = $('#banner');
 
 	// Breakpoints.
-	// breakpoints({
-	// 	xlarge: '(max-width: 1680px)',
-	// 	large: '(max-width: 1280px)',
-	// 	medium: '(max-width: 980px)',
-	// 	small: '(max-width: 736px)',
-	// 	xsmall: '(max-width: 480px)'
-	// });
+	breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)'
+	});
 
 	// Play initial animations on page load.
 	$window.on('load', function () {
